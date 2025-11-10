72c7a2d808e468de6f8ef0b8b4ba01bcbf396384
Rushikesh <rvp6123@gmail.com>
2025-11-10 13:25:16 +0530

    feat: add product detail page with dynamic product data

--- resources/views/product_details.blade.php
+++ resources/views/product_details.blade.php
@@ -1,102 +1,53 @@
-<!DOCTYPE html>
-<html>
+@extends('maindesign')
+<base href="/public">
+@section('product_details')
 
-<head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <link rel="shortcut icon" href="{{ asset('front_end/images/favicon.png') }}" type="image/x-icon">
-  <title>Product Details - Giftos</title>
-
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
-  <link rel="stylesheet" type="text/css" href="{{ asset('front_end/css/bootstrap.css') }}" />
-  <link href="{{ asset('front_end/css/style.css') }}" rel="stylesheet" />
-  <link href="{{ asset('front_end/css/responsive.css') }}" rel="stylesheet" />
-</head>
-
-<body>
-
-  <div class="hero_area">
-    <!-- header section -->
-    <header class="header_section">
-      <nav class="navbar navbar-expand-lg custom_nav-container">
-        <a class="navbar-brand" href="{{ route('index') }}">
-          <span>Giftos</span>
-        </a>
-
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
-          <span class=""></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav">
-            <li class="nav-item"><a class="nav-link" href="{{ route('index') }}">Home</a></li>
-          </ul>
-
-          <div class="user_option">
-            @if(Auth::check())
-              <a href="{{route('dashboard')}}"><i class="fa fa-user"></i> Dashboard</a>
-            @else
-              <a href="{{route('login')}}"><i class="fa fa-user"></i> Login</a>
-              <a href="{{route('register')}}"><i class="fa fa-user"></i> SignUp</a>
-            @endif
-            <a href="#"><i class="fa fa-shopping-bag"></i></a>
-          </div>
-        </div>
-      </nav>
-    </header>
-  </div>
-
-  <!-- product details section -->
-  <section class="shop_section layout_padding">
-    <div class="container">
-      <div class="heading_container heading_center">
-        <h2>Product Details</h2>
-      </div>
-
-      <div class="row justify-content-center">
-        <div class="col-md-6">
-          <div class="img-box text-center">
-            <img src="{{ asset('products/' . $product->product_image) }}" alt="{{ $product->product_title }}" style="width:100%; border-radius:10px;">
-          </div>
+        
+    {{-- Product Details Section --}}
+    <div class="container py-5">
+        <div style="max-width: 1000px; marging:0 auto; padding:20px;"> 
+          <button><a href="{{route('index')}}"> Back to Home</a></button>
         </div>
 
-        <div class="col-md-6">
-          <div class="detail-box">
-            <h3>{{ $product->product_title }}</h3>
-            <h5 class="text-muted">Category: {{ $product->product_category }}</h5>
-            <h4 class="text-danger mt-3">Price: ${{ $product->product_price }}</h4>
-            <h6 class="mt-3">Available Quantity: {{ $product->product_quantity }}</h6>
-
-            <p class="mt-4">{{ $product->product_description }}</p>
-
-            <div class="btn-box mt-4">
-              <a href="#" class="btn btn-primary">Add to Cart</a>
-              <a href="{{ route('index') }}" class="btn btn-secondary">Back to Shop</a>
+        <div class="row">
+            {{-- Product Image --}}
+            <div class="col-md-5 text-center">
+                <img src="{{ asset('products/'.$product->product_image) }}" 
+                     alt="{{ $product->product_title }}" 
+                     class="img-fluid rounded shadow"
+                     style="max-height: 400px;">
             </div>
-          </div>
+
+            {{-- Product Info --}}
+            <div class="col-md-7">
+                <h2 class="mb-3">{{ $product->product_title }}</h2>
+                <h5 class="text-muted mb-3">Category: {{ $product->product_category }}</h5>
+                <h3 class="text-success mb-3">$ {{ number_format($product->product_price, 2) }}</h3>
+                <h6 class="text-muted mb-3">Description:</h6>
+                <p class="mb-4">{{ $product->product_description }}</p>
+
+                {{-- Stock Info --}}
+                @if($product->product_quantity > 0)
+                    <p><strong class="text-success">In Stock:</strong> {{ $product->product_quantity }} available</p>
+                @else
+                    <p class="text-danger"><strong>Out of Stock</strong></p>
+                @endif
+
+                {{-- Add to Cart Form (optional) --}}
+                <form action="" method="POST">
+                    @csrf
+                    <div class="row align-items-center g-3">
+                        <div class="col-auto">
+                            <input type="number" name="quantity" value="1" min="1" max="{{ $product->product_quantity }}" class="form-control w-75">
+                        </div>
+                        <div class="col-auto">
+                            <button type="submit" class="btn btn-primary">Add to Cart</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
-      </div>
-
+        
     </div>
-  </section>
-
-  <!-- footer -->
-  <section class="info_section layout_padding2-top">
-    <footer class="footer_section">
-      <div class="container text-center">
-        <p>
-          &copy; <span id="displayYear"></span> All Rights Reserved By
-          <a href="https://html.design/">Web Tech Knowledge</a>
-        </p>
-      </div>
-    </footer>
-  </section>
-
-  <script src="{{ asset('front_end/js/jquery-3.4.1.min.js') }}"></script>
-  <script src="{{ asset('front_end/js/bootstrap.js') }}"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
-  <script src="{{ asset('front_end/js/custom.js') }}"></script>
-
-</body>
-</html>
+    
+@endsection>
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,6 @@ Route::get('/dashboard',[UserController::class,'index'] )->middleware(['auth', '
 
 
 
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');

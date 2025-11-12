1de823aada7854bc7826318f9958a91b544a1768
Rushikesh <rvp6123@gmail.com>
2025-11-12 09:39:13 +0530

    Improved All Products page layout with container and heading

--- resources/views/allproducts.blade.php
+++ resources/views/allproducts.blade.php
@@ -1,40 +1,45 @@
 @extends('maindesign')
 
 @section('all_products')
-    
-      <div class="row">
-        @foreach ($products as $product)
-        <div class="col-sm-6 col-md-4 col-lg-3">
-          <div class="box">
-            <a href="{{route('product_details', $product->id)}}">
-              <div class="img-box">
-                <img src="{{asset('products/'.$product->product_image)}}" alt="">
-              </div>
-              <div class="detail-box">
-                <h6>
-                  {{($product->product_title)}}
-                </h6>
-                <h6>
-                  Price
-                  <span>
-                    ${{($product->product_price)}}
-                  </span>
-                </h6>
-              </div>
-              <div class="new">
-                <span>
-                  New
-                </span>
-              </div>
-            </a>
+  <div class="container">
+          <div class="heading_container heading_center">
+            <h2>
+              All Products
+            </h2>
           </div>
-        </div>
+        <div class="row">
+        @foreach ($products as $product)
+          <div class="col-sm-6 col-md-4 col-lg-3">
+            <div class="box">
+              <a href="{{route('product_details', $product->id)}}">
+                <div class="img-box">
+                  <img src="{{asset('products/'.$product->product_image)}}" alt="">
+                </div>
+                <div class="detail-box">
+                  <h6>
+                    {{($product->product_title)}}
+                  </h6>
+                  <h6>
+                    Price
+                    <span>
+                      ${{($product->product_price)}}
+                    </span>
+                  </h6>
+                </div>
+                <div class="new">
+                  <span>
+                    New
+                  </span>
+                </div>
+              </a>
+            </div>
+          </div>
         @endforeach
-      </div>
-     <div class="btn-box">
-        <a href="{{route('index')}} ">
-          View Latest Products
-        </a>
-      </div>
-    </div>
+        </div>
+        <div class="btn-box">
+          <a href="{{route('index')}} ">
+            View Latest Products
+          </a>
+        </div>
+  </div>
 @endsection
